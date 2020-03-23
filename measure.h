83d253e27efbe72095c7c27a594dbe018fe95a3b
Karel Jilek <los.karlosss@gmail.com>
2020-03-23 18:55:54 +0100

    remove rdtsc initialization

--- measure.h
+++ measure.h
@@ -6,29 +6,12 @@
 
 #define REP 100
 
+// Returns the number of cycles required to run the function func
 unsigned long perf(fn func) {
     unsigned int cycles_low, cycles_high, cycles_low1, cycles_high1;
     unsigned long start, end;
     int i;
     unsigned long time[REP];
-
-    asm volatile("CPUID\n\t"
-                 "RDTSC\n\t"
-                 "mov %%edx, %0\n\t"
-                 "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::"%rax", "%rbx", "%rcx", "%rdx");
-    asm volatile("RDTSCP\n\t"
-                 "mov %%edx, %0\n\t"
-                 "mov %%eax, %1\n\t"
-                 "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::"%rax", "%rbx", "%rcx", "%rdx");
-    asm volatile("CPUID\n\t"
-                 "RDTSC\n\t"
-                 "mov %%edx, %0\n\t"
-                 "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::"%rax", "%rbx", "%rcx", "%rdx");
-    asm volatile("RDTSCP\n\t"
-                 "mov %%edx, %0\n\t"
-                 "mov %%eax, %1\n\t"
-                 "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::"%rax", "%rbx", "%rcx", "%rdx");
-
 
     for (i = 0; i < REP; i++) {
         asm volatile ("CPUID\n\t"
