745c37c73cc4deaf0136af133cde22817e52a696
olivier <olibitter@gmail.com>
2020-04-18 18:21:48 +0200

    Add command line args

--- plots/plot.py
+++ plots/plot.py
@@ -1,17 +1,22 @@
+#!/usr/bin/python3
+
 import os
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
-
-def extract_columns(data, x, y):
-    return data
+import csv_cols
 
 
-data = pd.read_csv("data/dummie_data.csv")
+if len(sys.argv) == 1 :
+    csv_file = "data/dummie_data.csv"
+
+
+data = pd.read_csv(csv_file)
 print(data.head())
 
 
 plt.rcParams.update(plt.rcParamsDefault)
 plt.style.use('ggplot')
 
-plt.plot(data["N"], data["PERFORMANCE"])
+plt.plot(data[csv_cols.PARAM_N], data[csv_cols.PERFORMANCE])
 plt.show()
