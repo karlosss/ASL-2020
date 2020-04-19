cf8ae4bfdd6d0370f8fd98ccfbaad76d5eb05113
olivier <olibitter@gmail.com>
2020-04-19 23:05:22 +0200

    Add 3 side by side plot

--- plots/plot.py
+++ plots/plot.py
@@ -44,6 +44,42 @@ def extract_data(data, x_axis, y_axis=csv_cols.PERFORMANCE, section="baum_welch"
     # plt.plot(extracted_data[x_axis], extracted_data[csv_cols.PERFORMANCE])
     # plt.show()    
 
+def plot_perf_NMT(data, title, N, M, T):
+    plt.figure(figsize=(20, 10), facecolor='w')
+    ax_N = plt.subplot(1, 3, 1)
+    ax_M = plt.subplot(1, 3, 2)
+    ax_T = plt.subplot(1, 3, 3)
+    plot_N_P(ax_N, data, "Plot N", M, T)
+    plot_M_P(ax_M, data, "Plot M", N, T)
+    plot_T_P(ax_T, data, "Plot T", N, M)
+    plt.show()
+    return
+
+def plot_N_P(ax, data, title, M, T):
+    ax.set_title(title)
+    plot_data(ax, data, csv_cols.PARAM_N, csv_cols.PERFORMANCE, section="baum_welch", M=M, T=T)
+
+    
+def plot_M_P(ax, data, title, N, T):
+    ax.set_title(title)
+    plot_data(ax, data, csv_cols.PARAM_M, csv_cols.PERFORMANCE, section="baum_welch", N=N, T=T)
+
+
+def plot_T_P(ax, data, title, N, M):
+    ax.set_title(title)
+    plot_data(ax, data, csv_cols.PARAM_M, csv_cols.PERFORMANCE, section="baum_welch", N=N, M=M)
+
+
+def plot_data(ax, data, x_axis, y_axis=csv_cols.PERFORMANCE, section="baum_welch", N=None, M=None, T=None):
+    extracted_data = extract_data(data, x_axis, y_axis, section, N, M, T)
+    # ax.set_title(extracted_data[csv_cols.BINARY].iloc[0])
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(f"Perf [F/C]", rotation=0)
+    ax.yaxis.set_label_coords(-0.05, 1.0)
+    ax.grid(axis='x')
+    ax.set_xticks(extracted_data[x_axis])
+    ax.plot(extracted_data[x_axis], extracted_data[y_axis])
+    
 
 
 def generate_dummie_values(binary, flags, N_range, M_range, T_range, iterations, sections):
@@ -103,11 +139,13 @@ if __name__ == "__main__":
 
     data = pd.read_csv('data/dummie_df.csv')
 
-    fixed_M = data[csv_cols.PARAM_M] == 6
-    fixed_T = data[csv_cols.PARAM_T] == 1
-    forward_vars = data[csv_cols.SECTION] == "forward_vars"
+    plt.rcParams.update(plt.rcParamsDefault)
+    plt.style.use('ggplot')
 
-    extract_data(data, csv_cols.PARAM_N)
+    plot_perf_NMT(data, "Performance Plot Variants", N=4, M=6, T=1)
+
+    
+
 
     
 
