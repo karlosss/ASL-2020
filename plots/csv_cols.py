da818fec9cfc55d8cc764b65064d57bdc346900f
olivier <olibitter@gmail.com>
2020-04-19 13:17:05 +0200

    Adapt csv col names

--- plots/csv_cols.py
+++ plots/csv_cols.py
@@ -1,12 +1,16 @@
 # CSV columns
-FLAGS = "FLAGS"
+FLAGS = "Flags"
 PARAM_N = "N"
 PARAM_M = "M"
 PARAM_T = "T"
-NUM_ITERATIONS = "NUM_ITERATIONS"
-BINARY = "BINARY"
-NUM_CYCLES = "CYCLES"
-NUM_FLOPS = "FLOPS"
+NUM_ITERATIONS = "Iterations"
+BINARY = "Binary"
+NUM_CYCLES = "Cycles"
+NUM_FLOPS = "Flops"
 DATA_MOVEMENT = "DATA_MOVEMENT"
-PERFORMANCE = "PERFORMANCE"
+PERFORMANCE = "Performance"
 MEMORY_BANDWIDTH = "BANDWIDTH"
+SECTION = "Section"
+
+
+CSV_HEADER='Binary, Flags, N, M, T, Iterations, Section, Cycles, Performance'
