e06e6e9ae3e81aa0851a0907e3b77811092297c7
olivier <olibitter@gmail.com>
2020-04-21 10:58:38 +0200

    Huge rework

--- plots/plot.py
+++ plots/plot.py
@@ -24,27 +24,118 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
-def extract_N_plot_data(data, M, T, section="baum_welch"):
-    M_constraint, T_constraint = data[csv_cols.PARAM_M] == M, data[csv_cols.PARAM_T] == T
-    return data[M_constraint & T_constraint]
+def extract_NP_data(data, M, T, section):
+    f =  data[
+          (data[csv_cols.PARAM_M] == M)
+        & (data[csv_cols.PARAM_T] == T)
+        & (data[csv_cols.SECTION] == section)
+    ]
+    return f[csv_cols.PARAM_N], f[csv_cols.PERFORMANCE]
 
-def extract_M_plot_data(data, N, T, section="baum_welch"):
-    N_constraint, T_constraint = data[csv_cols.PARAM_N] == N, data[csv_cols.PARAM_T] == T
-    return data[N_constraint & T_constraint]
+def extract_MP_data(data, N, T, section):
+    f =  data[
+          (data[csv_cols.PARAM_N] == N)
+        & (data[csv_cols.PARAM_T] == T)
+        & (data[csv_cols.SECTION] == section)
+    ]
+    return f[csv_cols.PARAM_M], f[csv_cols.PERFORMANCE]
+
+# def sections_data_generator_NP(data, M, T):
+#     return (extract_NP_data(data, M, T, section) for section in SECTIONS)
+    
+    
+# def sections_data_generator_NP(data, M, T):
+#     return (extract_NP_data(data, M, T, section) for section in SECTIONS)
+
+
+def comparison_data_generator_NP(data_generator, M, T):
+    return (extract_NP_data(data, M, T, "baum_welch") for data in data_generator)
+
+
+def plot_NP_sections(ax, data, M, T, sections=SECTIONS):
+    for section in sections:
+        x, y = extract_NP_data(data, M, T, section)
+        ax.set_xticks(x)
+        ax.plot(x, y, label=section)
+    # ax formatting
+    ax.set_title(f"M = {M}, T = {T}")
+    ax.set_xlabel(csv_cols.PARAM_N)
+    ax.set_ylabel(f"Perf [F/C]", rotation=0)
+    ax.yaxis.set_label_coords(-0.05, 1.0)
+
+    ax.grid(axis='x')
+    ax.legend(loc='lower right')
+
+
+def plot_MP_sections(ax, data, N, T, sections=SECTIONS):
+    for section in sections:
+        x, y = extract_MP_data(data, N, T, section)
+        ax.set_xticks(x)
+        ax.plot(x, y, label=section)
+    # ax formatting
+    ax.set_title(f"N = {N}, T = {T}")
+    ax.set_xlabel(csv_cols.PARAM_M)
+    ax.set_ylabel(f"Perf [F/C]", rotation=0)
+    ax.yaxis.set_label_coords(-0.05, 1.0)
+
+    ax.grid(axis='x')
+    ax.legend(loc='lower right')
+
+
+def get_experiment_info(data):
+    return data[csv_cols.BINARY].iloc[0], data[csv_cols.FLAGS].iloc[0]
+
+
+
+# def plot_NP_comparison(ax, data_it):
+#     # ax formatting
+#     for data in data_it:
+#         binary, _ = get_experiment_info(data)
+#         ax.plot(*extract_NP_data(data, M, T, section="baum_welch"), label=binary)
+
+
+
+def plot_NP_MP_S(data, title, N, M, T=None):
+    N = adjust_param(data, csv_cols.PARAM_N, N)
+    M = adjust_param(data, csv_cols.PARAM_M, M)
+    T = adjust_param(data, csv_cols.PARAM_T, T)
+    plt.figure(figsize=(15, 6), facecolor='w')
+    binary_name, flags = get_experiment_info(data)
+
+    fig = plt.gcf()
+    fig.suptitle(f"Binary: {binary_name}, Flags: {flags}", fontsize=16)
+
+    ax_NP = plt.subplot(1, 3, 1)
+    ax_MP = plt.subplot(1, 3, 2)
+    ax_S  = plt.subplot(1, 3, 3)
+ 
+    plot_NP_sections(ax_NP, data, M, T)
+    plot_MP_sections(ax_MP, data, N, T)
+
+
+    plot_regions_pie(ax_S, data, "Sections")
+
+    fig.tight_layout(pad=3.0, rect=[0, 0.0, 1, 0.95])
+    plt.show()
+    return
+
+# def extract_M_plot_data(data, N, T, section="baum_welch"):
+#     N_constraint, T_constraint = data[csv_cols.PARAM_N] == N, data[csv_cols.PARAM_T] == T
+#     return data[N_constraint & T_constraint]
     
 
-def extract_data(data, x_axis, y_axis=csv_cols.PERFORMANCE, section="baum_welch", N=None, M=None, T=None):
-    if x_axis == csv_cols.PARAM_N:
-        fixed_0, fixed_1 = data[csv_cols.PARAM_M] == M, data[csv_cols.PARAM_T] == T
+# def extract_data(data, x_axis, y_axis=csv_cols.PERFORMANCE, section="baum_welch", N=None, M=None, T=None):
+#     if x_axis == csv_cols.PARAM_N:
+#         fixed_0, fixed_1 = data[csv_cols.PARAM_M] == M, data[csv_cols.PARAM_T] == T
 
-    elif x_axis == csv_cols.PARAM_M: 
-        fixed_0, fixed_1 = data[csv_cols.PARAM_N] == N, data[csv_cols.PARAM_T] == T
+#     elif x_axis == csv_cols.PARAM_M: 
+#         fixed_0, fixed_1 = data[csv_cols.PARAM_N] == N, data[csv_cols.PARAM_T] == T
 
-    elif x_axis == csv_cols.PARAM_T:
-        fixed_0, fixed_1 = data[csv_cols.PARAM_N] == N, data[csv_cols.PARAM_M] == M
+#     elif x_axis == csv_cols.PARAM_T:
+#         fixed_0, fixed_1 = data[csv_cols.PARAM_N] == N, data[csv_cols.PARAM_M] == M
     
-    section_constraint = data[csv_cols.SECTION] == section
-    return  data[fixed_0 & fixed_1 & section_constraint]
+#     section_constraint = data[csv_cols.SECTION] == section
+#     return  data[fixed_0 & fixed_1 & section_constraint]
 
 
 def plot_regions_pie(ax, data, title):
@@ -62,6 +153,8 @@ def plot_regions_pie(ax, data, title):
     _, _, autotexts = ax.pie(region_perf[csv_cols.PERFORMANCE], labels=region_perf[csv_cols.SECTION], autopct='%1.1f%%')
     for autotext in autotexts:
         autotext.set_color('white')
+
+
 
 
 def adjust_param(data, param, value):
@@ -156,6 +249,13 @@ def plot_data(ax, data, x_axis, y_axis=csv_cols.PERFORMANCE, sections=["baum_wel
     ax.legend(loc='lower right')
 
 
+def plot_multiple(ax, xs, ys, labels):
+    return
+
+def plot(ax, x, y, label):
+    ax.plot(x, y, '-o', label=label)
+
+
 # def compare_data(csv_files):
 #     ax_N = plt.subplot(1, 2, 1)
 #     ax_M = plt.subplot(1, 2, 2)
@@ -226,9 +326,9 @@ if __name__ == "__main__":
     plt.rcParams.update(plt.rcParamsDefault)
     plt.style.use('ggplot')
 
-    plot_perf_NM_and_section_pie_chart(data, "Performance Plot Variants", N=27, M=37, all_sections=True)
+    # plot_perf_NM_and_section_pie_chart(data, "Performance Plot Variants", N=27, M=37, all_sections=True)
 
-    
+    plot_NP_MP_S(data, "Blubb",  N=27, M=37)
 
 
     
